f5c21f22beec09d64adbe63cb2cb7f696b1dfa98
Matthew Pancia <msp@themsp.org>
2018-01-01 14:57:10 -0800

    Add demographic information to graph.

--- va_districting/parse_data.py
+++ va_districting/parse_data.py
@@ -1,9 +1,12 @@
+"""Parse electoral data."""
+import os
+
+import igraph
 import numpy as np
 import pandas as pd
 import geopandas as gpd
-import igraph
+
 from rtree import index
-import os
 
 # Locations
 geojson_location = "../data/vaprecincts2013.geojson"
@@ -19,38 +22,43 @@ if __name__ == "__main__":
     geo_df = geo_df.set_index(geo_df["CODE"])
     demo_df = pd.read_csv(demo_location)
 
-    # Inspect data
-    # import IPython
-    # IPython.core.page.page("\n".join(demo_df.columns))
-
     # Pad code to correct length and set it to the index
     demo_df["CODE"] = demo_df["CODE"].astype(str).map(lambda x: x.zfill(7))
-    demo_df = demo_df.set_index(demo_df["CODE"]).rename(columns={
-        'Population' : 'population',
-        'Democratic' : 'surveyed_democrat',
-        'Independent' : 'surveyed_independent',
-        'Independent Green' : 'surveyed_independent_green',
-        'Libertarian' : 'surveyed_libertarian',
-        'Republican' : 'surveyed_republican',
-        'Grand Total' : 'surveyed_total',
-        '(blank)' : 'surveyed_blank'
-    })
+    demo_df = (demo_df
+               .set_index(demo_df["CODE"])
+               .rename(columns={
+                   'Population' : 'population',
+                   'Democratic' : 'surveyed_democrat',
+                   'Independent' : 'surveyed_independent',
+                   'Independent Green' : 'surveyed_independent_green',
+                   'Libertarian' : 'surveyed_libertarian',
+                   'Republican' : 'surveyed_republican',
+                   'Grand Total' : 'surveyed_total',
+                   '(blank)' : 'surveyed_blank'})
+               .assign(
+                   surveyed_democrat_percentage=lambda x: 100*x['surveyed_democrat'] / x['surveyed_total'],
+                   surveyed_independent_percentage=lambda x: 100*x['surveyed_independent'] / x['surveyed_total'],
+                   surveyed_independent_green_percentage=lambda x: 100*x['surveyed_independent_green'] / x['surveyed_total'],
+                   surveyed_libertarian_percentage=lambda x: 100*x['surveyed_libertarian'] / x['surveyed_total'],
+                   surveyed_blank_percentage=lambda x: 100*x['surveyed_blank'] / x['surveyed_total'],
+                   surveyed_republican_percentage=lambda x: 100*x['surveyed_republican'] / x['surveyed_total']
+               ))
 
     # Select wanted columns
     DEMO_COLUMNS = [
         'population',
-        'surveyed_democrat',
-        'surveyed_independent_green',
-        'surveyed_independent',
-        'surveyed_republican',
-        'surveyed_blank',
-        'surveyed_total',
-        'surveyed_libertarian'
+        'surveyed_democrat_percentage',
+        'surveyed_independent_green_percentage',
+        'surveyed_independent_percentage',
+        'surveyed_republican_percentage',
+        'surveyed_blank_percentage',
+        'surveyed_libertarian_percentage',
+        'surveyed_total'
     ]
     demo_df = demo_df[DEMO_COLUMNS]
 
     # Fill NaN with 0 for survey responses
-    demo_df = demo_df.fillna(0)
+    demo_df = demo_df.fillna(value={column: 0 for column in DEMO_COLUMNS if column != 'population'})
 
     # Join with geo data
     joined = geo_df.join(demo_df)
@@ -68,7 +76,7 @@ if __name__ == "__main__":
         joined[column] = joined[column].fillna(avg_value)
 
     idx = index.Index()
-    geo_buffer = geo_df.assign(geometry = geo_df.buffer(0))
+    geo_buffer = geo_df.assign(geometry=geo_df.buffer(0))
     for i, poly in geo_buffer.iterrows():
         idx.insert(int(i), poly.geometry.bounds)
 
