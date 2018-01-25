70d1944af0feaf47bd9b0d7c343219f19c0db073
Matthew Pancia <msp@themsp.org>
2018-01-25 13:51:21 -0800

    Add some more documentation.

--- va_districting/clustering.py
+++ va_districting/clustering.py
@@ -1,23 +1,20 @@
 """Classes for generating maps as clusters in precinct adjacency graph."""
+import itertools
+import logging
 import os
-import uuid
 import pickle
 import random
-import logging
-import itertools
-
-from typing import Dict, Set
+import uuid
 from collections import defaultdict
+from typing import Dict, Set
 
 import igraph
 import numpy as np
 import pandas as pd
-
 from igraph import Graph, Vertex
-from scipy.stats import entropy
-
 from parse_data import (GRAPH_LOCATION,
                         POLITICAL_COMPETITION_COLUMNS)
+from scipy.stats import entropy
 
 
 # pylint: disable=E1136,E1137,E1101,C0111,R0904
@@ -133,7 +130,16 @@ class Cluster(object):
         compactness = total_area / convex_hull_area
         return compactness
 
-    def add_vertex(self, vertex):
+    def add_vertex(self, vertex: Vertex):
+        """Add a vertex to the cluster.
+
+        Args:
+            vertex: A vertex from the graph to add to the cluster.
+
+        Returns:
+            This cluster.
+
+        """
         if vertex not in self.members:
             self.members = self.members.union(set([vertex]))
             self._needs_update['total_population'] = True
@@ -192,10 +198,13 @@ class Clustering(object):
         Properties:
             unused_vertices (Set[Vertex]): The set of vertices that belong to no cluster.
             used_vertices (Set[Vertex]): The set of vertices that belong to a cluster.
-            cluster_neighbors (Set[Vertex]): All of the vertices in the graph that are adjacent to some member of a given cluster.
-            unused_cluster_neighbors (Set[Vertex]): All of the vertices in the graph that are adjacent to some member of a given cluster and do not belong to a cluster.
+            cluster_neighbors (Set[Vertex]): All of the vertices in the graph that are adjacent to
+                some member of a given cluster.
+            unused_cluster_neighbors (Set[Vertex]): All of the vertices in the graph that are adjacent to some member
+                of a given cluster and do not belong to a cluster.
             cluster_lookup (dict): A dictionary that looks up the ID of the cluster that a given vertex belongs to.
-            cluster_component_counts (dict): A dictionary mapping cluster IDs to the number of connected components they have.
+            cluster_component_counts (dict): A dictionary mapping cluster IDs to the number
+                of connected components they have.
             cluster_sizes (dict): A dictionary mapping cluster IDs to the number of members they have.
             population_variance (float): The population variance of the cluster, as calculated in the method.
             political_entropy (float): The political entropy of the cluster, as calculated in the method.
@@ -290,6 +299,16 @@ class Clustering(object):
 
     @property
     def population_variance(self):
+        """Calculate the population variance of a clustering.
+
+        First, the average population size is calculated (over all clusters).
+        The variation of a cluster is measured as (population size / average population size) - 1.
+        The clustering variation is measured as the largest (in absolute value) of the variations over the clusters.
+
+        Returns:
+            float: The population variance of the clustering.
+
+        """
         if self._needs_update['population_variance']:
             population_sizes = np.array(list(self.cluster_sizes.values()))
             avg_population_size = np.mean(population_sizes)
@@ -301,6 +320,15 @@ class Clustering(object):
 
     @property
     def racial_dissimilarity(self):
+        """Calculate the racial dissimilarity of a clustering.
+
+        This is a calculation of the https://en.wikipedia.org/wiki/Index_of_dissimilarity
+        between the black and white population for each cluster, whose absolute values are summed up.
+
+        Returns:
+            float: The racial dissimilarity of the clustering.
+
+        """
         if self._needs_update['racial_dissimilarity']:
             cluster_differences = [cluster.racial_difference for cluster in self.clusters.values()]
             total_black_pop = np.sum([difference['total_black'] for difference in cluster_differences])
@@ -313,6 +341,14 @@ class Clustering(object):
 
     @property
     def political_entropy(self):
+        """Calculate the variance of the political entropies of the clusters in the clustering.
+
+        This is calculated as the standard deviation of the political entropies of the individual clusters in the clustering (for those with finite entropy).
+
+        Returns:
+            float: The political entropy of the clustering.
+
+        """
         if self._needs_update['political_entropy']:
             cluster_entropies = [cluster.political_entropy for cluster in self.clusters.values()]
             self._political_entropy = np.std([comp for comp in cluster_entropies if np.isfinite(comp)])
@@ -320,6 +356,13 @@ class Clustering(object):
         return self._political_entropy
 
     def compactness(self, geo_data):
+        """Calculate the average compactness score of the clusters in the clustering.
+
+        Requires geo data that specifies the shapes of the vertices.
+
+        Returns:
+            float: The average compactness score across the clusters in the clustering.
+        """
         compactness_scores = [cluster.compactness(geo_data) for cluster in self.clusters.values()]
         return np.mean(compactness_scores)
 
@@ -341,6 +384,15 @@ class Clustering(object):
         return self.clusters[cluster_id].neighbors
 
     def label_geo_data(self, geo_data):
+        """Add the clustering IDs for vertices to a shapefile.
+
+        Args:
+            geo_data: A geopandas DF with an index that aligns with the vertex IDs of the graph for this clustering.
+
+        Returns:
+            DataFrame: The geo_data DF with a cluster column that labels the vertices with their cluster ID.
+
+        """
         cluster_df = pd.DataFrame.from_dict(self.cluster_lookup, orient='index')
         cluster_df.columns = ['cluster']
         return geo_data.join(cluster_df)
@@ -362,6 +414,7 @@ class Clustering(object):
         return None
 
     def add_random_neighbor_to_cluster(self, cluster_id: int):
+        """Add a random neighbor to a given cluster, by ID."""
         neighbors = self.get_cluster_neighbors_by_id(cluster_id)
         unused_vertices = self.unused_vertices
         unused_neighbors = neighbors.intersection(unused_vertices)
@@ -379,27 +432,32 @@ class Clustering(object):
         return self
 
     def get_smallest_cluster_id(self):
+        """Find the smallest population cluster."""
         cluster_populations = list(self.cluster_sizes.items())
         cluster_populations.sort(key=lambda x: - x[1])
         smallest_cluster_id = cluster_populations.pop()[0]
         return smallest_cluster_id
 
     def grow_smallest_cluster(self):
+        """Find the smallest population cluster and add a random unused adjacent vertex to it."""
         smallest_cluster_id = self.get_smallest_cluster_id()
         return self.add_random_neighbor_to_cluster(smallest_cluster_id)
 
     def grow_to_min_size(self, min_size):
+        """Grow the clusters to have greater than the minimum cluster population size."""
         while min([x for x in list(self.cluster_sizes.values())]) < min_size:
             self.grow_smallest_cluster()
         return self
 
     def add_unused_neighbor(self):
+        """Randomly add an unused vertex to an adjacent cluster."""
         vertex = random.choice(list(self.unused_cluster_neighbors))
         vertex_neighbors = self.find_neighbor_for_vertex(vertex)
         cluster_to_add_to = random.choice(vertex_neighbors)
         return self.add_vertex_to_cluster_by_id(cluster_to_add_to, vertex)
 
     def save(self, location):
+        """Save the clustering."""
         graph_path = os.path.join(location, self.stub) + '.graph'
         self.graph.write_picklez(graph_path)
         lookup_path = os.path.join(location, self.stub) + '.clusters'
@@ -408,6 +466,7 @@ class Clustering(object):
         return self
 
     def copy(self):
+        """Create a copy of the clustering."""
         cluster_lookup = [(self.graph.vs.find(id), cluster_id) for id, cluster_id in self.cluster_lookup.items()]
         cluster_groups = itertools.groupby(cluster_lookup, lambda x: x[1])
         clusters = {id: Cluster(graph=self.graph, members=set([x[0] for x in members])) for id, members in
@@ -450,14 +509,14 @@ class Clustering(object):
 
     @classmethod
     def from_random_vertices(cls, seed_graph: Graph, num_clusters: int):
-        """ Generate a clustering by picking a random set of vertices.
-        """
+        """ Generate a clustering by picking a random set of vertices."""
         random_vertices = random.sample(list(seed_graph.vs), num_clusters)
         clusters = [Cluster(graph=seed_graph, members=set([vertex])) for vertex in random_vertices]
         return cls(seed_graph, {i: cluster for i, cluster in enumerate(clusters)})
 
     @classmethod
     def load(cls, location):
+        """Load a previously-saved clustering."""
         graph_path = location + '.graph'
         graph = Graph.Read_Picklez(graph_path)
         lookup_path = location + '.clusters'
@@ -469,14 +528,30 @@ class Clustering(object):
         return cls(graph=graph, clusters=clusters)
 
 
-def generate_map(component):
-    clustering = Clustering.from_random_vertices(component, 99)
+def generate_map(graph: Graph, num_clusters: int = 99, min_size: int = 30000, max_pop_variance: float = 5):
+    """Generate a seed clustering from a graph.
+
+    Args:
+        graph: A graph to cluster.
+        num_clusters: The number of clusters to generate.
+        min_size: The smallest acceptable population size for a cluster.
+        max_pop_variance: The maximum percentage population variance allowed for the clustering.
+
+    Returns:
+        Clustering: A clustering of the graph with num_clusters clusters.
+
+    """
+    # Generate an initial clustering from picking random initial vertices.
+    clustering = Clustering.from_random_vertices(graph, num_clusters)
+
+    # Grow to the minimum size.
     try:
-        clustering = clustering.grow_to_min_size(30000)
+        clustering = clustering.grow_to_min_size(min_size)
     except:
         logging.error('Generating min size failed.')
-        return generate_map(component)
+        return generate_map(graph)
 
+    # Add the unused vertices to the clusters. If this can't happen, retry.
     unused_vertices = len(clustering.unused_vertices)
     while unused_vertices > 0:
         try:
@@ -484,21 +559,18 @@ def generate_map(component):
             unused_vertices = len(clustering.unused_vertices)
         except:
             logging.error('Remainder allocation failed with {} unused vertices.'.format(unused_vertices))
-            return generate_map(component)
+            return generate_map(graph)
 
-    unused_vertices = len(clustering.unused_vertices)
-    if unused_vertices > 0:
-        logging.error('Remainder allocation failed with {} unused vertices.'.format(unused_vertices))
-        return generate_map(component)
-
+    # Check to make sure that the clustering has connected clusters. If not, retry.
     disconnected_count = np.sum([x for x in clustering.cluster_component_counts.values() if x > 1])
     if disconnected_count > 0:
         logging.error('{} disconnected components.'.format(disconnected_count))
-        return generate_map(component)
+        return generate_map(graph)
 
-    if clustering.population_variance > 5:
+    # Check to make sure that the clustering has acceptable population variance. If not, retry.
+    if clustering.population_variance > max_pop_variance:
         logging.error('Pop variance too high: {}'.format(clustering.population_variance))
-        return generate_map(component)
+        return generate_map(graph)
     else:
         return clustering
 
